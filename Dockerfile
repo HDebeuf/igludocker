ee9888fbf1a843ce6680596838ac3f18001ac142
HDebeuf <32390605+HDebeuf@users.noreply.github.com>
2018-05-16 22:33:31 +0200

    env var

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,10 @@ FROM debian:jessie-slim
 
 COPY baseWeb.sh /usr/local/bin
 
+ENV DB_ROOT_PASS
+ENV DB_USER
+ENV DB_USER_PASS
+
 RUN chmod +x /usr/local/bin/baseWeb.sh
 RUN ./usr/local/bin/baseWeb.sh
 
--- baseWeb.sh
+++ baseWeb.sh
@@ -2,7 +2,7 @@
 apt-get update && apt-get -y install --no-install-recommends openssh-server wget
 useradd newuser
 mkdir /var/run/sshd
-echo 'newuser:Tigrou007' | chpasswd
+echo "${DB_USER}:${DB_USER_PASS}" | chpasswd
 sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
 
@@ -16,9 +16,9 @@ apt-get update
 apt-get -y install --no-install-recommends php7.0 libapache2-mod-php7.0 php7.0-mysql php7.0-curl php7.0-json php7.0-gd php7.0-mcrypt php7.0-msgpack php7.0-memcached php7.0-intl php7.0-sqlite3 php7.0-gmp php7.0-geoip php7.0-mbstring php7.0-xml php7.0-zip
 
 #mysql
-apt-get -y install --no-install-recommends mariadb-server mariadb-client
-echo 'Tigrou007'
-echo 'Tigrou007'
+apt-get -y install --no-install-recommends mariadb-server
+${DB_ROOT_PASS}
+${DB_ROOT_PASS}
 
 #apache2
 apt-get -y install apache2
