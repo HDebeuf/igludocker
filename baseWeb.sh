02e3c9d91a16c8718567d7f096121df4c19956db
HDebeuf <32390605+HDebeuf@users.noreply.github.com>
2018-05-16 23:43:21 +0200

    use debconf

--- baseWeb.sh
+++ baseWeb.sh
@@ -16,8 +16,10 @@ apt-get update
 apt-get -y install --no-install-recommends php7.0 libapache2-mod-php7.0 php7.0-mysql php7.0-curl php7.0-json php7.0-gd php7.0-mcrypt php7.0-msgpack php7.0-memcached php7.0-intl php7.0-sqlite3 php7.0-gmp php7.0-geoip php7.0-mbstring php7.0-xml php7.0-zip
 
 #mysql
+apt-get install debconf-utils
+echo "mysql-server mysql-server/root_password password ${DB_ROOT_PASS}" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password  ${DB_ROOT_PASS}" | debconf-set-selections
 apt-get -y install --no-install-recommends mysql-server
-mysql_secure_installation
 
 #apache2
 apt-get -y install apache2
